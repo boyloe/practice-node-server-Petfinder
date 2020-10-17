e6309836895af00d3462922dc4eda2c3d6d2e1ad
boyloe <boyloe@gmail.com>
2020-10-17 10:21:00 -0600

    work on filter method for photos

--- index.js
+++ index.js
@@ -15,28 +15,29 @@ const options = {
     }
 };
 let token
+//parses the token out from the request
 const getToken = response => {
     token = response.data.access_token
     return token
 }
+
+//filter the animals for ones that have pictures
 const filterResponseForAnimalsWithPhotos = (response) => {
     return response.data.animals.filter(animal => animal.photos !== [] || animal.primary_photo_cropped !== null)
 }
-
+const showCatJson = (cats) => app.get('/', (_, res) => res.json(cats))
 axios.request(options).then(getToken).then(token => {
     axios.get('https://api.petfinder.com/v2/animals?type=cat&limit=100', {
         headers: {
             "Authorization": `Bearer ${token}`
         }
-    }).then(response => console.log(filterResponseForAnimalsWithPhotos(response)))
-    }).catch(error =>  console.error(error))
+    }).then(response => filterResponseForAnimalsWithPhotos(response))
+    .then(cats => showCatJson(cats))
+}).catch(error =>  console.error(error))
 
 
 
 
 
-app.get('/', (req, res) => {
-    res.json({message: 'hello'})
-}) 
 
 app.listen(port)
